90cb2fce547626eb9d41ee18ec8b8a75b2b356d0
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-18 21:24:14 -0300

    finalizado

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import Header from "../src/components/header.jsx"
+import Header from "./components/header/Header.js"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
